753b0f093a07c41d9929dc0ca5c4fb42b98975a8
Lumica <auroraveil14@gmail.com>
2025-12-22 20:53:16 +0530

    Page ready for deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,56 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Pages/Home";
-import Catalogue from "./Pages/Catalogue";
-import CategoryBooks from "./Pages/CategoryBooks";
-import Analytics from "./Pages/Analytics";
+import { lazy, Suspense } from "react";
 import Layout from "./Components/Layout";
-import NotFound from "./Pages/NotFound";
 
-const router = createBrowserRouter([
+const Home = lazy(() => import("./Pages/Home"));
+const Catalogue = lazy(() => import("./Pages/Catalogue"));
+const CategoryBooks = lazy(() => import("./Pages/CategoryBooks"));
+const Analytics = lazy(() => import("./Pages/Analytics"));
+const NotFound = lazy(() => import("./Pages/NotFound"));
+
+function PageLoader() {
+  return (
+    <div className="flex items-center justify-center h-64">Loading page...</div>
+  );
+}
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<PageLoader />}>
+          <Layout />
+        </Suspense>
+      ),
+
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "catalogue",
+          element: <Catalogue />,
+        },
+        {
+          path: "catalogue/:genre",
+          element: <CategoryBooks />,
+        },
+        {
+          path: "analytics",
+          element: <Analytics />,
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "catalogue",
-        element: <Catalogue />,
-      },
-      {
-        path: "catalogue/:genre",
-        element: <CategoryBooks />,
-      },
-      {
-        path: "analytics",
-        element: <Analytics />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+    basename: "/Library-Analytics",
+  }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
